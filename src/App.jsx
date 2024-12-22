98077b15cd4dfa72c2ae657bcf0e862fdaf3b73a
alikto <tgizemaleyna@gmail.com>
2024-12-22 18:24:54 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 bg-primary">
+      <div className="p-4 rounded shadow bg-light w-50 text-center">
+        <h1>Hello, World!</h1>
+        <p>This is a centered box with background.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App
